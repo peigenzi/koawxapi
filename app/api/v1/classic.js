334616ea691763159a77af0d4501892e33af4363
shenpeng <shenpengxp@outlook.com>
2019-06-04 22:56:47 +0800

    val

--- app/api/v1/classic.js
+++ app/api/v1/classic.js
@@ -1,16 +1,21 @@
 const Router = require('koa-router');
 const router = new Router();
 const { HttpException, ParameterException } = require('../../../core/http-exception.js');
+const { PositiveIntegerValidator } = require('../../validators/validator');
 
-router.get('/v1/classic/latest', (ctx, next) => {
-  if(1) {
-    const error = new ParameterException();
+router.post('/v1/:id/classic/latest', async (ctx, next) => {
+  const path = ctx.params;
+  const { query, headers, body } = ctx.request;
+  const v = await new PositiveIntegerValidator().validate(ctx);
 
-    throw error
-  }
+  // if (1) {
+  //   const error = new ParameterException();
+
+  //   throw error;
+  // }
 
   ctx.body = {
-    key: 'cal'
+    key: 'classic'
   };
 });
 
--- middlewares/exception.js
+++ middlewares/exception.js
@@ -1,8 +1,13 @@
 const { HttpException } = require('../core/http-exception');
+
 const catchError = async (ctx, next) => {
   try {
     await next();
   } catch (err) {
+    if (global.config.env === 'dev') {
+      throw err;
+    }
+
     if (err instanceof HttpException) {
       ctx.body = {
         msg: err.msg,
@@ -10,6 +15,13 @@ const catchError = async (ctx, next) => {
         request: `${ctx.method} ${ctx.path}`
       };
       ctx.status = err.code;
+    } else {
+      ctx.body = {
+        msg: 'network error',
+        error_code: 999,
+        request: `${ctx.method} ${ctx.path}`
+      };
+      ctx.status = 500;
     }
   }
 };
